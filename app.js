3e23492d15f5c36e7e91cb8493f0123d7adcad62
Brionna White <Brionnanwhite@gmail.com>
2016-11-04 15:13:04 -0700

    add

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ function insertionSort(arr) {
     val = arr[j = i];
     k = j - 1;
 
-    while(j && arr[k] > val) {
-      arr[j--] = arr[k--];
+    while(j && arr[k] > val) { // 1st loop: 1 && 1>3  true in while satement if its about true.
+      arr[j] = arr[k];
       arr[j] = val;
     }
   }
@@ -20,11 +20,11 @@ console.log(insertionSort(arr));
 
 
 // VARIABLE LOOP VALUE
-// (lim)   3 | 3
+// (lim)   3 | 3 lim stay 3 .
 // (i)     1 | 2
 // (j)     1 | 2
 // (k)     0 | 1
 // (val)   3 | 2
 
 
-(for, while, swamp)
+(for, while, swap)
